3ad0c58327f6cc3cf1748df7328e73b462936172
ish-xyz <ishamaraia@gmail.com>
2022-09-24 15:58:57 +0200

    Implemented downloader, needs to be integrated

--- pkg/node/downloader/downloader.go
+++ pkg/node/downloader/downloader.go
@@ -2,38 +2,45 @@ package downloader
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"sync"
 
 	"github.com/sirupsen/logrus"
 )
 
 type Downloader struct {
-	mu            sync.Mutex
-	Queue         []*Item
-	EmergencyStop bool
+	mu         sync.Mutex
+	Queue      []*Item
+	KillSwitch bool
+	Client     *http.Client
+	Logger     *logrus.Entry
 }
 
 type Item struct {
-	Path string
-	URL  string
+	Req      *http.Request
+	FilePath string
 }
 
-func NewDownloader() *Downloader {
+func NewDownloader(log *logrus.Entry) *Downloader {
 
 	return &Downloader{
-		Queue:         []*Item{},
-		EmergencyStop: false,
+		Queue:      []*Item{},
+		KillSwitch: false,
+		Logger:     log,
+		Client:     &http.Client{},
 	}
 }
 
-func (d *Downloader) Push(url, path string) {
+func (d *Downloader) Push(req *http.Request, filepath string) {
 
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
 	item := &Item{
-		Path: path,
-		URL:  url,
+		Req:      req,
+		FilePath: filepath,
 	}
 
 	d.Queue = append(d.Queue, item)
@@ -54,17 +61,48 @@ func (d *Downloader) Pop() (*Item, error) {
 	return nil, fmt.Errorf("empty queue")
 }
 
-func (d *Downloader) Watch(log *logrus.Entry) error {
+func (d *Downloader) Watch() error {
 	for {
-		if d.EmergencyStop {
+		if d.KillSwitch {
 			break
 		}
 
 		if len(d.Queue) > 0 {
-			lastItem, _ := d.Pop()
-			log.Infoln("processing item", lastItem)
-			fmt.Println(lastItem)
+			lastItem, err := d.Pop()
+			if err != nil {
+				d.Logger.Errorln("failed to take last item of the queue")
+				continue
+			}
+			d.download(lastItem)
 		}
 	}
 	return nil
+}
+
+func (d *Downloader) download(item *Item) error {
+
+	file, err := os.Create(item.FilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	resp, err := d.Client.Do(item.Req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	size, err := io.Copy(file, resp.Body)
+
+	if resp.Header.Get("content-length") != fmt.Sprintf("%d", size) {
+		d.Logger.Errorln("content-length and file size don't match, deleting file")
+		err = os.Remove(item.FilePath)
+		if err != nil {
+			d.Logger.Errorln("failed to delete file:", err)
+		}
+		return err
+	}
+
+	return err
 }
